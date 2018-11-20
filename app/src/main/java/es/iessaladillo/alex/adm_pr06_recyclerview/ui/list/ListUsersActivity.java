6d8d6a6f92d528a27a7024226e465ea7f36e158b
Alejandro <alesitonightray@gmail.com>
2018-11-20 20:14:26 +0100

    ListUsersActivity dataBinding and creation of viewModel

--- app/src/main/java/es/iessaladillo/alex/adm_pr06_recyclerview/ui/list/ListUsersActivity.java
+++ app/src/main/java/es/iessaladillo/alex/adm_pr06_recyclerview/ui/list/ListUsersActivity.java
@@ -4,13 +4,21 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProviders;
 import es.iessaladillo.alex.adm_pr06_recyclerview.R;
+import es.iessaladillo.alex.adm_pr06_recyclerview.databinding.ActivityListBinding;
+import es.iessaladillo.alex.adm_pr06_recyclerview.local.UserDatabase;
 
 public class ListUsersActivity extends AppCompatActivity {
+
+    private ActivityListBinding b;
+    private ListUsersActivityViewModel viewModel;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list);
+        b = DataBindingUtil.setContentView(this, R.layout.activity_list);
+        viewModel = ViewModelProviders.of(this, new ListUsersActivityViewModelFactory(UserDatabase.getInstance())).get(ListUsersActivityViewModel.class);
     }
 }
